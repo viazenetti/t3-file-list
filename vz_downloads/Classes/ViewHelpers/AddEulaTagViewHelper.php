13d54efb1d2c038d9836a46c66d8a7bef4fb4628
Christian Ludwig <cl@viazenetti.de>
2014-06-24 11:48:05 +0200

    EULA für ZIP angepasst, dass auch übergeordnete PlugIns berücksichtigt werden

--- vz_downloads/Classes/ViewHelpers/AddEulaTagViewHelper.php
+++ vz_downloads/Classes/ViewHelpers/AddEulaTagViewHelper.php
@@ -6,24 +6,33 @@ class AddEulaTagViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractView
 
 	/**
 	 * @param string $id
-     * @param array $downloads
+     * @param boolean $getAllIds
+     * @param boolean $resetIds
 	 * @return string
 	 */
-	public function render($id = NULL, $downloads = NULL) {
+	public function render($id = NULL, $getAllIds = false, $resetIds = false) {
 
-		if ((Int)$id > 0) {
-            return ' data-eula-id="' . intval($id) . '"';
+        if (!isset($GLOBALS['T3_VAR']['EXT']['tx_vzdownloads']['eulaIds'])) {
+            $GLOBALS['T3_VAR']['EXT']['tx_vzdownloads']['eulaIds'] = array();
         }
-        elseif (is_array($downloads)) {
-            $ids = array();
-            foreach ($downloads as $download) {
-                if ($download['tx_vzdownloads_euladata'] > 0) {
-                    $ids[] = $download['tx_vzdownloads_euladata'];
-                }
-            }
-            if (count($ids) > 0) {
-                return ' data-eula-id="' . implode(',', $ids) . '"';
-            }
+
+        if ((int)$id > 0) {
+            $ids = intval($id);
+            $GLOBALS['T3_VAR']['EXT']['tx_vzdownloads']['eulaIds'][] = $ids;
         }
+
+        if ($getAllIds) {
+            $ids = implode(',', array_unique($GLOBALS['T3_VAR']['EXT']['tx_vzdownloads']['eulaIds'], SORT_NUMERIC));
+        }
+
+        if ($resetIds) {
+            unset($GLOBALS['T3_VAR']['EXT']['tx_vzdownloads']['eulaIds']);
+        }
+
+        if (isset($ids)) {
+            return ' data-eula-id="' . $ids . '"';
+        }
+
+        return '';
 	}
 }
